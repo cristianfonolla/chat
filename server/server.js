4f223a95026a6a93aadbea38fa8dfdac841844db
cristian <cristian.fonolla@gmail.com>
2017-03-21 21:43:48 +0100

    added socket.io-client

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ var Redis = require('ioredis');
 var redis = new Redis();
 
 redis.psubscribe('*', function(err, count) {
-    console.log('Errors subscribing to channel');
+    console.log('Suscribed to'+count);
+
+    if (err){
+        console.log("Error");
+    }
 });
 
 redis.on('pmessage', function(subscribed,channel, message) {
