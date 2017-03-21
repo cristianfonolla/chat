cd4a5cb8ddd742798e2e7d66afcee072f50937af
cristian <cristian.fonolla@gmail.com>
2017-03-21 20:29:07 +0100

    added server

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -16,8 +16,8 @@ class AdminUserSeeder extends Seeder
     {
         try {
             factory(App\User::class)->create([
-                    "name" => "Sergi Tur Badenas",
-                    "email" => "sergiturbadenas@gmail.com",
+                    "name" => "cristian",
+                    "email" => "cristian.fonolla@gmail.com",
                     "password" => bcrypt(env('ADMIN_PWD', '123456'))]
             );
         } catch (\Illuminate\Database\QueryException $exception) {
