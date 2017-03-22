b342d0fb6eb259f04f42d2d329b00582b1ddc262
cristian <cristian.fonolla@gmail.com>
2017-03-22 10:29:40 +0100

    client updated

--- client/client.js
+++ client/client.js
@@ -3,8 +3,8 @@ socket.on('connect', function(){
     console.log('Client connected ok to localhost port 3000')
 });
 socket.on('chat', function(data){
-    console.log('Event chat receive')
-    console.log(data);
+    console.log('Event chat received')
+    console.log(data)
 });
 socket.on('disconnect', function(){
     console.log('Client disconnected ok from localhost port 3000')
